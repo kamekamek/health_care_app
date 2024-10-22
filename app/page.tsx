1cd3e1c08dc1f8b7f099265da770da714f641d62
kamekamek <nagare.0913@gmail.com>
2024-10-22 22:17:02 +0900

    supabaseのインストール

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import { Page as AppPage } from "@/components/app-page"
+// import { createClient } from '@/utils/supabase/supabase';
 
-export default function Page() {
+export function Page() {
   return <AppPage />
+}
+
+export default async function Notes() {
+  const supabase = createClient();
+  const { data: notes } = await supabase.from("notes").select();
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
