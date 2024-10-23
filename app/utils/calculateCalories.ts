93c3761b262b330644ca2e1d0911bd016f2a0859
kamekamek <nagare.0913@gmail.com>
2024-10-23 22:37:46 +0900

    一旦保存

--- app/utils/calculateCalories.ts
+++
@@ -1,6 +0,0 @@
-export const calculateCalories = (currentWeight: number, targetWeight: number) => {
-    const weightDifference = currentWeight - targetWeight;
-    const dailyCaloricDeficit = 500; // 1日500カロリーの減少を目指す
-    const suggestedCalories = 2000 - (weightDifference * dailyCaloricDeficit / 7); // 基本的なカロリー計算
-    return suggestedCalories;
-};
